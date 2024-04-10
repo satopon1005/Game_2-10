e1abb637b2fbd84b5205946bffe3cd7852f4d031
Satopon105 <145268614+Satopon105@users.noreply.github.com>
2024-04-11 00:47:01 +0900

    update

--- src/Objects/Enemy/Enemy.cpp
+++ src/Objects/Enemy/Enemy.cpp
@@ -7,10 +7,9 @@ void Enemy::Init() {
 	m_handle = LoadGraph(ENEMY_PATH);
 	m_pos.x = 100.0f;
 	m_pos.y = 100.0f;
-	m_speedX = 10.0f;
+	m_speed = 10.0f;
 	m_hp = 1;
 	m_flg = true;
-	m_turn = 1;
 
 }
 
@@ -18,14 +17,14 @@ void Enemy::Init() {
 void Enemy::Step() {
 
 	if (m_pos.x >= SCREEN_SIZE_X) {
-		m_turn *= -1;
+		m_speed *= -1;
 		m_pos.y += 10.0f;
 	}
 	else if (m_pos.x <= 0) {
-		m_turn *= -1;
+		m_speed *= -1;
 		m_pos.y += 10.0f;
 	}
-	m_pos.x += m_speedX * m_turn;
+	m_pos.x += m_speed;
 
 }
 
--- src/Objects/Enemy/Enemy.h
+++ src/Objects/Enemy/Enemy.h
@@ -6,8 +6,7 @@ class Enemy
 {
 private:
 	int m_handle;			// ハンドル
-	VECTOR m_pos;
-	float m_posX, m_posY;	// 座標
+	VECTOR m_pos;			// 座標
 	float m_speedX;			// 移動速度
 	int m_hp;				// 体力
 	bool m_flg;				// 生存フラグ
@@ -19,10 +18,10 @@ public:
 	void Draw();
 	void Fin();
 
-	float GetPosX() { return m_posX; };	// X座標取得
-	float GetPosY() { return m_posY; };	// Y座標取得
+	float GetPosX() { return m_pos.x; };	// X座標取得
+	float GetPosY() { return m_pos.y; };	// Y座標取得
 
-	void SetPosX(float x) { m_pos.x = x; };				// X座標設定
-	void SetPosY(float y) { m_pos.y = y; };				// Y座標設定
+	void SetPosX(float x) { m_pos.x = x; };	// X座標設定
+	void SetPosY(float y) { m_pos.y = y; };	// Y座標設定
 
 };
