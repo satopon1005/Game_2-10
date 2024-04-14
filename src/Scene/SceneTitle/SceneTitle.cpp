f46cf44bf5566ed05979fbf127df49e249f7598c
hiretusama <144640173+hiretusama@users.noreply.github.com>
2024-04-15 02:43:15 +0900

    タイトルの点滅処理

--- src/Scene/SceneTitle/SceneTitle.cpp
+++ src/Scene/SceneTitle/SceneTitle.cpp
@@ -6,10 +6,23 @@
 void SceneTitle::Init()
 {
 	bg_handle = LoadGraph(TITLE_BG_HANDLE_PATH);
+	Enterhandle = LoadGraph(ENTER_HANDLE_PATH);
 }
 
 bool SceneTitle::Step()
 {
+
+	DrawGraph(0, 0, Titlehandle, true);
+
+	//100‰ñ‚Ì‚¤‚¿20‰ñ•\¦‚·‚é(“_–Åˆ—)
+	static int count = 0;
+	count = (count + 1) % 100;
+	if (count < 20) {
+
+		//•\¦
+		DrawGraph(0, 100, Enterhandle, true);
+	}
+
 	if (Input::IsKeyPush(KEY_INPUT_RETURN)) {
 		return true;
 	}
@@ -19,9 +32,11 @@ bool SceneTitle::Step()
 void SceneTitle::Draw()
 {
 	DrawGraph(0, 0, bg_handle, true);
+	DrawGraph(0, 0, Enterhandle, true);
 }
 
 void SceneTitle::Fin()
 {
 	DeleteGraph(bg_handle);
+	DeleteGraph(Enterhandle);
 }
--- src/Scene/SceneTitle/SceneTitle.h
+++ src/Scene/SceneTitle/SceneTitle.h
@@ -1,12 +1,13 @@
 #pragma once
 
 constexpr char TITLE_BG_HANDLE_PATH[] = { "data/Scene/Title/Titlescreen.png" };
+constexpr char ENTER_HANDLE_PATH = {"data/Scene/Title/Enter.png"};
 
 class SceneTitle
 {
 private:
 	int bg_handle;
-
+	int Enterhandle;
 public:
 	void Init();
 	bool Step();
